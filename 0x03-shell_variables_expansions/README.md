300a02a18b885338ce02e3c025422dc6dffed501
aliceada96 <aliceada96@gmail.com>
2023-09-25 15:14:41 +0300

    Revert "Configuration done"
    This reverts commit 0ba99501ed7d8e4fb9347e3476f2634f5259def9.

---
+++ 0x03-shell_variables_expansions/README.md
@@ -0,0 +1,3 @@
+0 create alias
+1 hello you
+
--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# create a file in /tmp. with specified requirements:
-file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
