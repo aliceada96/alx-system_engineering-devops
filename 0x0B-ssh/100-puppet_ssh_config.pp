1c4a7102bb64a620d61637bc62752444605f5421
aliceada96 <aliceada96@gmail.com>
2023-09-26 18:32:22 +0300

    updated server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # creates an ssh config file on server
 file { '/etc/ssh/ssh_config':
   ensure  => present,
-  content => "#!/usr/bin/env bash\n# Configuration file for my local ssh client\n# Allows connection to server without password\nHost 100.26.227.25\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication no",
+  content => "#!/usr/bin/env bash\n# Configuration file for my local ssh client\n# Allows connection to server without password\nHost 100.25.161.102\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication no",
 }
