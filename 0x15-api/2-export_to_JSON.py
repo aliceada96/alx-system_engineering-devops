8a847eb054dcacbac917c2a75fbaf0852f067345
aliceada96 <aliceada96@gmail.com>
2023-10-24 01:59:24 +0300

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """This script exportd data to json"""
-from sys import argv
+
 import json
 import requests
-
+from sys import argv
 
 if __name__ == "__main__":
 
     user_id = argv[1]
-
-    username = requests.get(
-          f"https://jsonplaceholder.typicode.com/users/{argv[1]}").json()
-    ['username']
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
 
     to_dos = requests.get(
           f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos")
-
-    completed = [i for i in to_dos.json() if i['completed'] is True]
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
