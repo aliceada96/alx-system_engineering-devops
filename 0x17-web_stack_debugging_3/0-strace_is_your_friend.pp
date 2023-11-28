238f4a618e398b32e088de8f3a794bcbcaed15cf
aliceada96 <aliceada96@gmail.com>
2023-11-28 14:16:15 +0300

    webstack debugging-strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+}
